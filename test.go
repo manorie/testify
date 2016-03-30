38416d149b1235fa49205aa490fdcaadaf2347c3
manorie <mcetin.cm@gmail.com>
2016-03-30 23:47:45 +0300

    some stuff.

--- test.go
+++ test.go
@@ -1,37 +1,50 @@
 package testify
 
 import (
+	"math/rand"
 	"strconv"
 	"time"
 )
 
 type Test struct {
-	id             uint64
-	title          string
-	author         string
-	path           string
-	authorEmail    string
-	isPublished    bool
-	timeLimit      uint8
-	answerSize     uint8
-	creationDate   uint64
-	expirationDate uint64
+	Id             uint64
+	Title          string
+	Author         string
+	Path           string
+	SecretKey      string
+	AuthorEmail    string
+	IsPublished    bool
+	TimeLimit      uint8
+	AnswerSize     uint8
+	CreationDate   int64
+	ExpirationDate int64
 }
 
-func CreateEmptyTest() (*Test, error) {
+func CreateNewTest() (*Test, error) {
+	path, _ := constructAUniqueKey()
+	secretKey, _ := constructAUniqueKey()
+
 	return &Test{
-		title: "alfa",
+		Path:         path,
+		SecretKey:    secretKey,
+		IsPublished:  false,
+		CreationDate: time.Now().Unix(),
 	}, nil
 }
 
-func ConstructASafePath() (string, error) {
-	time := strconv.FormatInt(time.Now().Unix(), 10)
-	t := []rune(time)
-	x := make([]rune, 10, 20)
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz-_~")
+
+func constructAUniqueKey() (string, error) {
+	now := strconv.FormatInt(time.Now().Unix(), 10)
+	t := []rune(now)
+	x := make([]rune, 20, 20)
+
+	ns := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(ns)
 
 	for _, r := range t {
+		x = append(x, letterRunes[r1.Intn(len(letterRunes))])
 		x = append(x, r)
-		x = append(x, 'x')
 	}
 	return string(x), nil
 }
--- test_test.go
+++ test_test.go
@@ -1,11 +1,3 @@
 package testify
 
-import (
-	"testing"
-)
-
-func TestCreateEmptyTest(t *testing.T) {
-	test, _ := ConstructASafePath()
-
-	t.Errorf("%v", test)
-}
+import ()
