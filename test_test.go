a0ca919f0e90f5be603a422c1340bd2bb31d97bd
manorie <mcetin.cm@gmail.com>
2016-04-06 21:50:17 +0300

    improve test.

--- test_test.go
+++ test_test.go
@@ -7,6 +7,10 @@ import (
 func TestAddAuthor(t *testing.T) {
 	test, _ := CreateNewTest()
 	test.AddAuthor("Alfonso")
-
-	t.Errorf("%v", test)
+	test.AddTitle("Back to the future")
+	test.AddAuthorEmail("mcetin.cm@gmail.com")
+	test.SetTimeLimit(20)
+	test.SetAnswerSize(4)
+	test.SetExpireInDays(10)
+	//t.Errorf("%v", test)
 }
